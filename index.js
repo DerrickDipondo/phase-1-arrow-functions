5bb8098c43419d9c621f7bcfdf05c596ea240347
DerrickDipondo <derrickdipondo@gmail.com>
2024-11-29 20:32:07 +0300

    arrow_functions

---
+++ index.js
@@ -0,0 +1,15 @@
+//Normal function expression
+function divide(a = 2000, b =100) {
+    return  a/b;
+}
+console.log(divide());
+
+//Arrow function declaration
+const square = x => x * 2;
+console.log(square(2));
+
+//Another Arrow function illustration
+const add = (a, b) => a + b;
+console.log(add(3, 4));
+
+
